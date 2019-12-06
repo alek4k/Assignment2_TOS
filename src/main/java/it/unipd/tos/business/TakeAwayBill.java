24972a46ad64d779f0f8add5c37281b9bb5e66ea
Alessandro Lovo <alessandro.lovo@studenti.unipd.it>
2019-12-06 17:16:18 +0100

    close #7 creazione interfaccia TakeAwayBill

---
+++ src/main/java/it/unipd/tos/business/TakeAwayBill.java
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////
+// Alessandro Lovo 1142682
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos.business;
+
+import it.unipd.tos.business.exception.TakeAwayBillException;
+import it.unipd.tos.model.MenuItem;
+
+import java.util.List;
+
+public interface TakeAwayBill {
+    double getOrderPrice(List<MenuItem> itemsOrdered) throws TakeAwayBillException;
+}
